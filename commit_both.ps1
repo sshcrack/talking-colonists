c10dd72ba286001f0af2eb63cdf02b46cd9e32e8
sshcrack <34072808+sshcrack@users.noreply.github.com>
2025-06-20 15:19:04 +0200

    Add pws1 helper scripts

---
+++ commit_both.ps1
@@ -0,0 +1,22 @@
+$msg=$args[0]
+$activeBranch = git rev-parse --abbrev-ref HEAD
+
+$switchToBranch = 'forge-1.20.1'
+
+if($activeBranch -eq "forge-1.20.1") {
+    $switchToBranch = 'neoforge-1.21.1'
+}
+
+$ErrorActionPreference = 'Stop'
+
+
+
+git commit -m $msg
+$hash = git rev-parse HEAD
+
+git push
+git checkout $switchToBranch
+git cherry-pick $hash
+git push
+
+git checkout $activeBranch
