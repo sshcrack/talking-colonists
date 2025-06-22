2cb092118e2b5687f13e409c501a31cec35dce4e
sshcrack <34072808+sshcrack@users.noreply.github.com>
2025-06-22 18:09:34 +0200

    Add build all script

---
+++ build_all.ps1
@@ -0,0 +1,37 @@
+$activeBranch = git rev-parse --abbrev-ref HEAD
+
+$gradleProperties = Get-Content -Path "gradle.properties" | Where-Object { $_ -match "^mod_version=" }
+$modVersion = ($gradleProperties -split "=")[1].Trim()
+Write-Host "Mod version from gradle.properties: $modVersion"
+
+
+$switchToBranch = 'forge-1.20.1'
+
+if($activeBranch -eq "forge-1.20.1") {
+    $switchToBranch = 'neoforge-1.21.1'
+}
+
+$ErrorActionPreference = 'Stop'
+
+if (-not (Test-Path "both_loaders")) {
+    mkdir  "both_loaders"
+}
+
+.\gradlew clean build
+if($activeBranch -eq "forge-1.20.1") {
+    Move-Item "./build/libs/*-all.jar" "both_loaders/mc_talking_$modVersion-forge-1.20.1.jar" -Force
+} else {
+    Move-Item "./build/libs/*.jar" "both_loaders/mc_talking_$modVersion-neoforge-1.21.1.jar" -Force
+}
+
+git checkout $switchToBranch
+.\gradlew clean build
+
+if($switchToBranch -eq "forge-1.20.1") {
+    Move-Item "./build/libs/*-all.jar" "both_loaders/mc_talking_$modVersion-forge-1.20.1.jar" -Force
+} else {
+    Move-Item "./build/libs/*.jar" "both_loaders/mc_talking_$modVersion-neoforge-1.21.1.jar" -Force
+}
+
+git checkout $activeBranch
+Move-Item "both_loaders" "build/both_loaders" -Force
