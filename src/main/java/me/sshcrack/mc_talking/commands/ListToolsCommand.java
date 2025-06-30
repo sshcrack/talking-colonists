2f22c53400c06f3eeda10609d960ab239296fcb7
sshcrack <34072808+sshcrack@users.noreply.github.com>
2025-06-30 16:12:35 +0200

    remove old gson references and update functionality of list_tools

--- src/main/java/me/sshcrack/mc_talking/commands/ListToolsCommand.java
+++ src/main/java/me/sshcrack/mc_talking/commands/ListToolsCommand.java
@@ -1,29 +1,82 @@
 package me.sshcrack.mc_talking.commands;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.IntegerArgumentType;
-import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
+import me.sshcrack.gemini_live_lib.gson.properties.EnumProperty;
+import me.sshcrack.gemini_live_lib.gson.properties.ObjectProperty;
+import me.sshcrack.gemini_live_lib.gson.properties.PrimitiveProperty;
+import me.sshcrack.mc_talking.manager.tools.AITools;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
+
+import java.io.StringWriter;
 
 public class ListToolsCommand {
-    private static int run(int page, CommandContext<CommandSourceStack> context) {
+    private static final SimpleCommandExceptionType NO_TOOLS = new SimpleCommandExceptionType(Component.translatable("mc_talking.commands.no_tools_available"));
+    private static final SimpleCommandExceptionType TOOL_NOT_FOUND = new SimpleCommandExceptionType(Component.translatable("mc_talking.commands.tool_not_found"));
+
+    private static int get_description(CommandContext<CommandSourceStack> context, String toolName) throws CommandSyntaxException {
         var src = context.getSource();
-        return 1;
+
+        var tool = AITools.registeredFunctions.get(toolName);
+        if (tool == null) {
+            throw TOOL_NOT_FOUND.create();
+        }
+
+        var strWriter = new StringWriter();
+        var prop = tool.getProperty();
+        if (prop instanceof ObjectProperty objProp) {
+            objProp.getProperties().forEach((key, value) -> {
+                strWriter.write(key + ": ");
+                if (value instanceof PrimitiveProperty prim) {
+                    strWriter.write(prim.getType());
+                } else if (value instanceof EnumProperty enumP) {
+                    var gson = new Gson();
+                    var s = new StringWriter();
+                    gson.toJson(enumP, s);
+                    var elem = JsonParser.parseString(s.toString());
+
+                    var arr = elem.getAsJsonObject().get("enum").getAsJsonArray();
+                    strWriter.write(arr.toString());
+                } else {
+                    strWriter.write("Unsupported type");
+                }
+                //TODO Add array
+
+                strWriter.write("\n");
+            });
+        }
+
+        src.sendSuccess(() -> Component.translatableEscape("mc_talking.commands.tool_description", toolName, tool.getDescription(), strWriter.toString()), true);
+        return 0;
     }
 
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
-        dispatcher.register(
-                Commands.literal("list_tools")
-                        .executes(context -> {
-                            return run(0, context);
-                        })
-                        .then(RequiredArgumentBuilder.argument("page", IntegerArgumentType.integer(0)))
-                        .executes(context -> {
-                            var page = IntegerArgumentType.getInteger(context, "page");
-                            return run(page, context);
-                        })
-        );
+        var builder = Commands.literal("list_tools");
+
+        for (String fn_name : AITools.getRegisteredFunctionNames()) {
+            builder
+                    .then(Commands.literal(fn_name)
+                            .executes(context -> get_description(context, fn_name)));
+        }
+
+        dispatcher.register(builder
+                .requires(source -> source.hasPermission(2))
+                .executes(ctx -> {
+                    var src = ctx.getSource();
+                    var tools = AITools.getRegisteredFunctionNames();
+
+                    if (tools.isEmpty()) {
+                        throw NO_TOOLS.create();
+                    }
+
+                    src.sendSuccess(() -> Component.translatableEscape("mc_talking.commands.list_tools", String.join(", ", tools)), true);
+                    return 1;
+                }));
     }
 }
