4f97edc9a232141972cf00b728e84c12713653b3
Hendrik Lind <hendrik@sshcrack.me>
2026-05-01 15:10:59 +0200

    fix: also be able to end conversations with the player

--- src/main/java/me/sshcrack/mc_talking/manager/tools/EndConversationAction.java
+++ src/main/java/me/sshcrack/mc_talking/manager/tools/EndConversationAction.java
@@ -4,14 +4,13 @@ import com.google.gson.JsonObject;
 import com.minecolonies.api.colony.IColony;
 import com.minecolonies.api.entity.citizen.AbstractEntityCitizen;
 import me.sshcrack.mc_talking.ConversationManager;
-import me.sshcrack.mc_talking.conversations.LiveConversationWsClient;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class EndConversationAction extends FunctionAction {
     public EndConversationAction() {
         super("end_conversation", """
-                This will end the conversation between you and your peer. Your peer isn't able to respond after this tool has been called.
+                This will end the conversation. The peer you are talking to isn't able to respond after. Only use if you are sure you want to end the conversation.
                 """);
     }
 
@@ -21,14 +20,7 @@ public class EndConversationAction extends FunctionAction {
         var client = ConversationManager.getClientForEntity(citizen.getUUID());
         var obj = new JsonObject();
 
-        if (!(client instanceof LiveConversationWsClient liveClient) || liveClient.getPeer() == null) {
-            obj.addProperty("success", false);
-            obj.addProperty("error", "You are in a conversation with a player. Can not end conversation.");
-            return obj;
-        }
-
-
-        liveClient.endConversationWhenPossible();
+        client.endConversationWhenPossible();
         obj.addProperty("success", true);
 
         return obj;
